e4b7706979d8b6ece3331452758253a8cad5d453
ClemGamer <tsaiyouchen@gmail.com>
2025-06-14 21:22:52 +0800

    Complete the loop to ask AI code agent and let it fixed the code of calcaulator

--- main.py
+++ main.py
@@ -29,38 +29,48 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
-    generate_content(client, messages, verbose)
+    print(generate_content(client, messages, verbose))
 
 
 def generate_content(client, messages, verbose):
 
-    response = client.models.generate_content(
-        model="gemini-2.0-flash-001",
-        contents=messages,
-        config=types.GenerateContentConfig(
-            tools=[available_functions],
-            system_instruction=system_prompt,
-        ),
-    )
+    for i in range(20):
+        response = client.models.generate_content(
+            model="gemini-2.0-flash-001",
+            contents=messages,
+            config=types.GenerateContentConfig(
+                tools=[available_functions],
+                system_instruction=system_prompt,
+            ),
+        )
 
-    if not response.function_calls:
-        return response.text
+        if not response.function_calls:
+            return response.text
 
-    for function_call_part in response.function_calls:
-        # print(f"Calling function: {function_call_part.name}({function_call_part.args})")
-        function_call_result = call_function(function_call_part, verbose)
-        if (
-            not function_call_result.parts
-            or not function_call_result.parts[0].function_response
-        ):
-            raise Exception("Error call function: {function_call_part}")
+        if response.candidates:
+            for candidate in response.candidates:
+                messages.append(candidate.content)
+
+        for function_call_part in response.function_calls:
+            # print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+            function_call_result = call_function(function_call_part, verbose)
+            if (
+                not function_call_result.parts
+                or not function_call_result.parts[0].function_response
+            ):
+                raise Exception("Error call function: {function_call_part}")
+            messages.append(function_call_result)
+            if verbose:
+                print(f"-> {function_call_result.parts[0].function_response.response}")
+
         if verbose:
-            print(f"-> {function_call_result.parts[0].function_response.response}")
+            if response.usage_metadata is not None:
+                print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+                print(
+                    f"Response tokens: {response.usage_metadata.candidates_token_count}"
+                )
 
-    if verbose:
-        if response.usage_metadata is not None:
-            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    return "over 20 times"
 
 
 if __name__ == "__main__":
